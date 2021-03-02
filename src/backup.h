bde00d8e2deb73d09d928f2618ad63074aae86b7
jun-yeoung <dbahsmssk@naver.com>
2021-03-02 23:56:06 +0900

    20210302

--- src/backup.h
+++ src/backup.h
@@ -1,3 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <pwd.h>
+#include <grp.h>
+#include <pthread.h>
+#include <time.h>
 
--- src/main.c
+++ src/main.c
@@ -1,13 +1,3 @@
-#include <string.h>
-#include <stdbool.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <dirent.h>
-#include <pwd.h>
-#include <grp.h>
-#include <pthread.h>
-#include <time.h>
 #include "backup.h"
 
 typedef struct backuplist {
@@ -68,31 +58,28 @@ int is_valid_order (char order[]) {
 // 쓰레드함수로 만들어서 주기마다 카피 실행.
 //병렬적으로 백업이 되어야 하기 때문.
 // 
-/*
-void* copy_file (void* arg) {
-
-	chdir(argv[1]); // 백업  디렉토리폴더로 이동.
+void* copy_file (void *filelist) {
 
 	int count;
 	char buffer [5] = { 0, };
 	char backupname[200] = {0, };
-	char time [50] = {0, };
-	
+	char chtime [50] = {0, };
+	list* bcklist = (void*)filelist;
 	pthread_mutex_lock(&mutex);
-	
+
 	time_t t;
 	struct tm* timeinfo;
 
 	time(&t);
 	timeinfo = localtime(&t);
 
-	strcpy(time,asctime(timeinfo));
+	strcpy(chtime,asctime(timeinfo));
 
-	strcpy(backupname,bcklist->filelist);
-	strcat(backupname,time);
+	strcpy(backupname,bcklist->filename);
+	strcat(backupname,chtime);
 
-	FILE* src = fopen("tokenlist[1]","r");
-	FILE* dest = fopen("backupname","w");
+	FILE* src = fopen(bcklist->filename,"r");
+	FILE* dest = fopen(backupname,"w");
 
 	while (feof(src) == 0) {
 		count = fread(buffer,sizeof(char),4,src);
@@ -104,22 +91,34 @@ void* copy_file (void* arg) {
 	fclose(src);
 
 	pthread_mutex_unlock(&mutex);
-}*/
-void* search_list (void* head) {
-	printf("검색 스레드 실행 된 유무확인");
-/*	while(1){
+}
+void* search_list (void *head) {
+	while(1){	
 		list* cur = (list*)head;
 		while (cur->next != NULL) {
 			cur = cur->next;
-			printf("%s",cur->filename);
-
+			pthread_t backup;
+			pthread_create(&backup,NULL,copy_file,(void*)cur);
+			printf("실행확인\n");
 		}
-	}*/
+	}
 }
-
-void prompt (char* argv) {
-
+/*
+   void* remove_list (void* target){						
+   list* front_target;
+   while(front_target->next != NULL){
+   front_target = front_target->next;
+   if (strcmp(front_target->next->filename,tokenlist[1]) == 0) {
+   front_target->next = front_target->next->next;
+   }
+   }
+   }
+ */
+void prompt () {
+	pthread_t search;
+			
 	while (1) {
+		pthread_create(&search,NULL,search_list,(void*)head);
 		char order[500] = {0,};
 		char tokenlist[6][300] = {0,};
 		int i = 0;
@@ -142,49 +141,47 @@ void prompt (char* argv) {
 
 		switch (a) 
 		{
-			case 0: {	
-						if (tokenlist[1] == " ") {
-							printf("ERROR if  디버깅\n");
-							printf("ERROR 공백 \n");
+			case 0: { if (tokenlist[1] == " ") {
+						printf("ERROR if  디버깅\n");
+						printf("ERROR 공백 \n");
+						break;
+					}
+					if (!is_basic_file(tokenlist[1])) { // file에 대한 유효성 검사.
+						printf("ERROR 파일 유효성\n");
+						break;
+					}
+
+					list* cur;
+					cur = head;
+					while (cur->next != NULL) {
+						if (strcmp(cur->filename,tokenlist[1]) == 0){
+							printf("ERROR 이미 존재\n");
 							break;
 						}
-						if (!is_basic_file(tokenlist[1])) { // file에 대한 유효성 검사.
-							printf("ERROR 파일 유효성\n");
-							break;
-						}
+						cur = cur->next; // 리스트 중요한 부분.. 잘 기억하기
+					}
+					// 이미 기존 리스트에 존재하는지 검사.
+					// filename에 대한 유효성검사 끝.
+					if (tokenlist[2] == " "){
+						printf("ERROR 주기가 입력안됨\n");
+					}
+					double period = atof(tokenlist[2]);
+					int turncated = (int)period;
+					if (period != turncated || turncated == 0) {
+						printf("ERROR 정수형이아님\n");
+						break;
+					}
+					//period 유효성 검사 끝.
+					list* bcklist = (list*)malloc(sizeof(list));
 
-						list* cur;
-						cur = head;
-						while (cur->next != NULL) {
-							if (strcmp(cur->filename,tokenlist[1]) == 0){
-								printf("ERROR 이미 존재\n");
-								break;
-							}
-							cur = cur->next; // 리스트 중요한 부분.. 잘 기억하기
-						}
-						// 이미 기존 리스트에 존재하는지 검사.
-						// filename에 대한 유효성검사 끝.
-						if (tokenlist[2] == " "){
-							printf("ERROR 주기가 입력안됨\n");
-						}
-						double period = atof(tokenlist[2]);
-						int turncated = (int)period;
-						if (period != turncated || turncated == 0) {
-							printf("ERROR 정수형이아님\n");
-							break;
-						}
-						//period 유효성 검사 끝.
-						list* bcklist = (list*)malloc(sizeof(list));
+					strcpy(bcklist->filename,tokenlist[1]); //file이름 넣어주기.
+					bcklist->period = turncated; // 주기 넣어주기
 
-						strcpy(bcklist->filename,tokenlist[1]); //file이름 넣어주기.
-						bcklist->period = turncated; // 주기 넣어주기
+					bcklist->next = head->next; // 리스트간 연결.
+					head->next = bcklist;
 
-						bcklist->next = head->next; // 리스트간 연결.
-						head->next = bcklist;
-
-						break;}
-			case 1: { 
-						list* front_target;
+					break;}
+			case 1: { 	list* front_target;
 						while(front_target->next != NULL){
 							front_target = front_target->next;
 							if (strcmp(front_target->next->filename,tokenlist[1]) == 0) {
@@ -198,8 +195,7 @@ void prompt (char* argv) {
 
 			case 3:
 
-			case 4:{
-					   list* temp;
+			case 4:{ list* temp;
 					   temp = head;
 					   while (temp->next != NULL) {
 						   temp = temp->next;
@@ -229,13 +225,14 @@ int main(int argc,char* argv[]) {
 	char foldername [8] = "/backup";
 	char strbuffer [300] = {0,};
 	char* pstrbuffer = NULL;
-	
+
 	if (argc == 1) { // 입력이 안되면,
 
 		pstrbuffer = getcwd(strbuffer,300); // 현재 디렉토리 경로얻기.
 		strcat(strbuffer,foldername); // 현재 디렉토리경로 + 디렉토리이름
 
 		int dir_result = mkdir(strbuffer,0755);
+		chdir(strbuffer);
 
 	}
 	else { // 경로입력이 되었다면,
@@ -259,11 +256,11 @@ int main(int argc,char* argv[]) {
 				return 0;
 			}
 		}
+	chdir(argv[1]);	
 	}
+	
 	head = (list*)malloc(sizeof(list));
 	head->next = NULL;
-	pthread_t search;
-	prompt(argv[1]);
-	pthread_create(&search,NULL,search_list,/*(void*)head*/NULL);
+	prompt();
 
 }
